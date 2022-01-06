b8b02b27259ad4daaa56d22f2755d03f6cdde60f
George S <george.lmfao@gmail.com>
2022-01-06 21:41:59 +0000

    Rearranged financial data into a main() while loop.

--- index.js
+++ index.js
@@ -2,46 +2,138 @@
 const fetch = require("node-fetch");
 
 // Database Variables
-const Database = require("@replit/database");
-const db = new Database();
+const Client = require("@replit/database");
+const client = new Client();
 
 // API variables
 const base = "api.coinlayer.com";
 const apiKey = process.env['apiKey'];
 const target = "USD";
 const symbol = "XMR";
+const LOOP_DELAY = 2000;
 
-// Financial data 
-let balanceUSD = 1000.00;
-let balanceXMR = 0.0;
 
-// Time data in UTC
-let rightNow;
+// wrap all the code we want to run in a main() function
+async function main() {
 
-while(true){
+    // use this to setup initial data
+    // await setupData();
 
-    rightNow = new Date();
+    // Financial data 
+    let balanceUSD = await client.get("balanceUSD");
+    let balanceXMR = await client.get("balanceXMR");
 
-    if ( isTimeToTrade(rightNow) ) {
-        console.log("It's time to trade! Dolla billz y'all!");
-    } else {
-        console.log("It's not time to trade yet.");
+    // Time data in UTC
+    let rightNow;
+    let hasYetToTrade = true;
+
+    await client.getAll().then(something => console.log("getAll()", something));
+
+    await client.list().then(keys => { console.log(keys) });
+
+    while (true) {
+        console.log("looping..");
+        if (buyConditions(hasYetToTrade)) {
+            console.log("It's time to trade! Three Dolla billz y'all!");
+            buy({ balanceUSD, balanceXMR });
+        }
+        else if (skipSellConditions(hasYetToTrade)) {
+            console.log("It's time to trade but I've already traded.");
+        }
+        else if (sellConditions()) {
+            sell();
+        }
+        else {
+            hasYetToTrade = !hasYetToTrade ? true : false;
+        }
+
+        await delay(LOOP_DELAY);
     }
-
 }
 
+async function setupData() {
+    await client.set("balanceXMR", 0.0);
+    await client.set("balanceUSD", 1000.0);
+}
 
-function setPeak( peakDate ) {
+function buyConditions(hasYetToTrade) {
+    rightNow = new Date();
+    return isTimeToTrade(rightNow) && hasYetToTrade;
+}
+
+function skipSellConditions(hasYetToTrade) {
+    rightNow = new Date();
+    return isTimeToTrade(rightNow) && !hasYetToTrade;
+}
+
+function sellConditions() {
+    return false;
+}
+
+function sell() {
+    return false;
+}
+
+function setPeak(peakDate) {
     fetch(`http://${base}/${peakDate}?access_key=${apiKey}&target=${target}&symbols=${symbol}`)
-        .then( (res) => { return res.json() } )
-        .then( (data) => { return data.rates.XMR } )
-        .then( (rate) => { db.set("peak", rate) } )
-        .catch( (err) => { console.log(`I am getting an error: ${err}`) } );
+        .then((res) => { return res.json() })
+        .then((data) => { return data.rates.XMR })
+        .then((rate) => { client.set("peak", rate) })
+        .catch((err) => { console.log(`I am getting an error: ${err}`) });
 }
 
-function isTimeToTrade( rightNow ) {
-    let rightHour = rightNow.getHours() === 15 || rightNow.getHours() === 19 || rightNow.getHours() === 23;
+function isTimeToTrade(rightNow) {
+    let rightHour = rightNow.getHours() === 15 ||
+        rightNow.getHours() === 19 ||
+        rightNow.getHours() === 23;
     let rightMinute = rightNow.getMinutes() === 0;
     let rightSecond = rightNow.getSeconds() === 0;
     return rightHour && rightMinute && rightSecond;
 }
+
+async function updateXMRPrice() {
+    fetch(`http://${base}/live?access_key=${apiKey}&target=${target}&symbols=${symbol}`)
+        .then((res) => { return res.json() })
+        .then((json) => { client.set("lastQuotedPrice", `${json.rates.XMR}`) })
+        .then(() => { console.log("The Price of XMR has been updated.") })
+        .catch((err) => { console.log("There was an error fetching prices.") });
+}
+
+async function buy({ balanceUSD, balanceXMR }) {
+    client.get("lastQuotedPrice").then((lastQuotedPrice) => {
+        let purchaseQuantity = Math.floor(balanceUSD / lastQuotedPrice * 100000) / 100000;
+        let power = 0;
+        console.log(`Initial USD Balance ${balanceUSD}`);
+        console.log(`Price of 1 XMR: $${lastQuotedPrice} USD`);
+        console.log(`Purchase Quantity: ${purchaseQuantity} XMR`);
+
+        while (balanceUSD > 0.02 && power <= 5) {
+
+            let qtyToBuy = Math.floor(purchaseQuantity * (Math.pow(10, power))) / Math.pow(10, power);
+            balanceXMR += qtyToBuy;
+            let qtyPriceUSD = Math.ceil(lastQuotedPrice * qtyToBuy * 100) / 100;
+            balanceUSD -= qtyPriceUSD;
+            balanceUSD = Math.floor(qtyPriceUSD * 100) / 100;
+            purchaseQuantity -= qtyToBuy;
+
+            console.log(`I am buying ${qtyToBuy} XMR for ${qtyPriceUSD} USD`);
+            console.log(`New XMR Balance: ${balanceXMR}`);
+            console.log(`New USD Balance: ${balanceUSD}`);
+            console.log(`New Quantity to buy: ${purchaseQuantity}`);
+            console.log(`Purchase Quantity precision: ${power}`);
+            power++;
+        }
+
+        client.set("balanceUSD", balanceUSD);
+        client.set("balanceXMR", balanceXMR);
+    });
+}
+
+// util functions
+async function delay(ms) {
+    // return await for better async stack trace support in case of errors.
+    return await new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// call our main function, should be only function ran by ths script
+main();
